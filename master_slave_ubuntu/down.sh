a00353030b17e7ec69513eb404e0f752f450a1f8
Ashry Ramadhan <ashry.ramadhan@webscientia.id>
2024-05-24 15:26:32 +0700

    ADD Master Slave Ubuntu

---
+++ master_slave_ubuntu/down.sh
@@ -0,0 +1,5 @@
+sudo docker compose stop
+sudo docker compose down
+sudo docker ps -a
+sudo docker image rm master_slave_ubuntu-jenkins-agent1
+sudo docker images
---
+++ master_slave_ubuntu/run.sh
@@ -0,0 +1,3 @@
+sudo docker compose create
+sudo docker compose start
+sudo docker compose ps -a
